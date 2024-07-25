137a0a535126d3e41797cde5bd3b1dfaacf10b5a
mathxlee <math.lee@qq.com>
2024-07-25 15:32:22 +0800

    feat: update maintenance time

--- lib/utils.ts
+++ lib/utils.ts
@@ -86,7 +86,7 @@ export const getNetworkUrl = (networkId: any) => {
 
 export const maintenanceStartTime = "2024-07-25T16:00:00+08:00";
 
-export const maintenanceEndTime = "2024-07-25T18:30:00+08:00";
+export const maintenanceEndTime = "2024-07-25T18:00:00+08:00";
 
 export const maintenanceNetworks = ["devnet", "testnet"];
 
