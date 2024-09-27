b05f970c654e6cf10b3c58768d25210388419156
mathxlee <math.lee@qq.com>
2024-09-27 18:06:44 +0800

    feat: extend wallet campaign to oct 13th

--- lib/utils.ts
+++ lib/utils.ts
@@ -122,7 +122,7 @@ export const isMobileDevice = () => {
 };
 
 export const walletCampaignStartTime = "2024-08-30T10:00:00+08:00";
-export const walletCampaignEndTime = "2024-09-29T10:00:00+08:00";
+export const walletCampaignEndTime = "2024-10-13T10:00:00+08:00";
 export const walletCampaignNetworks = ["devnet"];
 
 export const isInWalletCampaignTime = (networkId = "devnet") => {
