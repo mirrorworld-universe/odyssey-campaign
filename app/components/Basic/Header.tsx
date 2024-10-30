88e678a63c52605a1a3322c089036309e8f3c728
jack pan <jackpanyj@gmail.com>
2024-10-30 13:45:43 +0800

    feat: replace icon

--- app/components/Basic/Header.tsx
+++ app/components/Basic/Header.tsx
@@ -200,57 +200,64 @@ export function Header() {
   }, [networkId]);
 
   return (
-    <nav className="flex flex-col w-full sticky sticky:backdrop-blur-[35px] top-0 z-30">
-      <div className="h-16 md:h-20 flex items-center justify-between p-4 md:px-4 xl:px-10 md:py-4 bg-black w-full transition-all duration-300">
-        {/* left */}
-        <div className="flex items-center gap-3 md:gap-8">
-          {/* logo */}
-          <Link href="/" className="inline-flex flex-row items-center gap-2">
-            <img alt="Sonic Logo" className="size-8 min-w-8" src="/sonic.png" />
-            <span className="hidden md:inline text-white text-[22px] font-bold font-orbitron tracking-widest">
-              SONIC
+    <div className="bg-black w-full sticky sticky:backdrop-blur-[35px] top-0 z-30">
+      <nav className="flex max-w-[1440px] mx-auto flex-col w-full">
+        <div className="h-16 md:h-20 flex items-center justify-between p-4 md:px-10 md:py-4 bg-black w-full transition-all duration-300">
+          {/* left */}
+          <div className="flex items-center gap-3 md:gap-8">
+            {/* logo */}
+            <Link href="/" className="inline-flex flex-row items-center gap-2">
+              <img
+                alt="Sonic Logo"
+                className="size-8 min-w-8"
+                src="/sonic.png"
+              />
+              <span className="hidden md:inline text-white text-[22px] font-bold font-orbitron tracking-widest">
+                SONIC
+              </span>
+            </Link>
+
+            {/* menu */}
+            <span
+              className="size-8 inline-flex md:hidden cursor-pointer"
+              onClick={() => setShowMenu(true)}
+            >
+              <IconMenu color="white" />
             </span>
-          </Link>
 
-          {/* menu */}
-          <span
-            className="size-8 inline-flex md:hidden cursor-pointer"
-            onClick={() => setShowMenu(true)}
-          >
-            <IconMenu color="white" />
-          </span>
-
-          {/* nav */}
-          <div
-            className={cn(
-              "flex flex-col md:flex-row fixed items-center md:static top-0 right-0 bottom-0 left-0 m-auto z-30 bg-[#111] w-full h-full md:w-auto md:h-auto duration-300 transition-transform",
-              showMenu ? "translate-x-0" : "-translate-x-full md:translate-x-0"
-            )}
-          >
-            <div className="w-full flex md:hidden p-4 justify-between items-center border-b border-solid border-white/10">
-              <span className="text-[#666] text-base font-orbitron font-semibold">
-                Menu
-              </span>
-              <span
-                className="cursor-pointer"
-                onClick={() => setShowMenu(false)}
-              >
-                <IconClose width={24} height={24} color="#4D4D4D" />
-              </span>
-            </div>
-
-            {/* switch network */}
-            {(visitedNetworkId === "testnet" ||
-              networkCookieSearch === NETWORK_COOKIE_NAME ||
-              networkCookie === "frontier") && (
-              <div className="w-full md:w-auto p-4 md:p-0">
-                <NetworkSwitch />
+            {/* nav */}
+            <div
+              className={cn(
+                "flex flex-col md:flex-row fixed items-center md:static top-0 right-0 bottom-0 left-0 m-auto z-30 bg-[#111] w-full h-full md:w-auto md:h-auto duration-300 transition-transform",
+                showMenu
+                  ? "translate-x-0"
+                  : "-translate-x-full md:translate-x-0"
+              )}
+            >
+              <div className="w-full flex md:hidden p-4 justify-between items-center border-b border-solid border-white/10">
+                <span className="text-[#666] text-base font-orbitron font-semibold">
+                  Menu
+                </span>
+                <span
+                  className="cursor-pointer"
+                  onClick={() => setShowMenu(false)}
+                >
+                  <IconClose width={24} height={24} color="#4D4D4D" />
+                </span>
               </div>
-            )}
 
-            {/* spliter */}
-            <div className="w-px h-4 bg-white/20 hidden md:block"></div>
-            {/* <i
+              {/* switch network */}
+              {(visitedNetworkId === "testnet" ||
+                networkCookieSearch === NETWORK_COOKIE_NAME ||
+                networkCookie === "frontier") && (
+                <div className="w-full md:w-auto p-4 md:p-0">
+                  <NetworkSwitch />
+                </div>
+              )}
+
+              {/* spliter */}
+              <div className="w-px h-4 bg-white/20 hidden md:block"></div>
+              {/* <i
               className={cn(
                 "hidden md:inline-flex h-4 w-[1px] border-solid border-white/20 md:mx-6 2xl:mx-8",
                 visitedNetworkId === "testnet" ||
@@ -261,66 +268,66 @@ export function Header() {
               )}
             ></i> */}
 
-            <div className="w-full md:w-auto flex flex-col md:flex-row items-start md:items-center gap-8">
-              {menu.map((menuItem, menuIndex) => (
-                <Link
-                  className={cn(
-                    "gap-12 text-title3 hover:text-gold-yellow text-primary font-orbitron transition-colors px-4 py-5 md:px-0 md:py-0",
-                    pathname.startsWith("/task") && menuItem.link === "/task"
-                      ? "text-[#FBB042]"
-                      : ""
-                  )}
-                  href={menuItem.link[networkId || "devnet"]}
-                  key={menuIndex}
-                  target={menuItem.target}
-                  onClick={handleClickMenu}
-                >
-                  {menuItem.name}
-                </Link>
-              ))}
+              <div className="w-full md:w-auto flex flex-col md:flex-row items-start md:items-center gap-8">
+                {menu.map((menuItem, menuIndex) => (
+                  <Link
+                    className={cn(
+                      "gap-12 text-title3 hover:text-gold-yellow text-primary font-orbitron transition-colors px-4 py-5 md:px-0 md:py-0",
+                      pathname.startsWith("/task") && menuItem.link === "/task"
+                        ? "text-[#FBB042]"
+                        : ""
+                    )}
+                    href={menuItem.link[networkId || "devnet"]}
+                    key={menuIndex}
+                    target={menuItem.target}
+                    onClick={handleClickMenu}
+                  >
+                    {menuItem.name}
+                  </Link>
+                ))}
+              </div>
             </div>
+          </div>
+
+          {/* right */}
+          <div className="gap-6 md:gap-10 flex items-center">
+            {address && token ? <RingPopover /> : null}
+
+            {address && token ? <Notification /> : null}
+
+            {!publicKey ? (
+              <Button
+                className={cn(
+                  "font-orbitron text-title3 text-primary",
+                  isInMaintenance
+                    ? "hover:bg-[#0000FF] opacity-30 cursor-not-allowed"
+                    : "hover:bg-[#0000FF]/80 active:bg-[#0000FF]/60 cursor-pointer"
+                )}
+                variant={"primary"}
+                size={isMobile ? "sm" : "md"}
+                onClick={handleClickOpenWallet}
+              >
+                {connecting ? "Connecting..." : "Connect Wallet"}
+              </Button>
+            ) : (
+              <UserDropdown />
+            )}
           </div>
         </div>
 
-        {/* right */}
-        <div className="gap-6 md:gap-10 flex items-center">
-          {address && token ? <RingPopover /> : null}
-
-          {address && token ? <Notification /> : null}
-
-          {!publicKey ? (
-            <Button
-              className={cn(
-                "font-orbitron text-title3 text-primary",
-                isInMaintenance
-                  ? "hover:bg-[#0000FF] opacity-30 cursor-not-allowed"
-                  : "hover:bg-[#0000FF]/80 active:bg-[#0000FF]/60 cursor-pointer"
-              )}
-              variant={"primary"}
-              size={isMobile ? "sm" : "md"}
-              onClick={handleClickOpenWallet}
-            >
-              {connecting ? "Connecting..." : "Connect Wallet"}
-            </Button>
-          ) : (
-            <UserDropdown />
-          )}
-        </div>
-      </div>
-
-      {/* system notification bar */}
-      {isInMaintenance && isOpenNotificationBar && (
-        <NotificationBar className="bg-[#00063C] min-h-12 h-auto px-3">
-          <div className="inline-flex justify-center gap-2 text-base text-[#48BBFF] font-semibold py-3">
-            <Speaker
-              width={24}
-              height={24}
-              color="#48BBFF"
-              className="min-w-6 min-h-6"
-            />
-            <span className="inline-flex w-full max-w-[718px] whitespace-nowrap overflow-hidden">
-              {/* upgrade */}
-              {/* <div className="pl-[100%] animate-marquee">
+        {/* system notification bar */}
+        {isInMaintenance && isOpenNotificationBar && (
+          <NotificationBar className="bg-[#00063C] min-h-12 h-auto px-3">
+            <div className="inline-flex justify-center gap-2 text-base text-[#48BBFF] font-semibold py-3">
+              <Speaker
+                width={24}
+                height={24}
+                color="#48BBFF"
+                className="min-w-6 min-h-6"
+              />
+              <span className="inline-flex w-full max-w-[718px] whitespace-nowrap overflow-hidden">
+                {/* upgrade */}
+                {/* <div className="pl-[100%] animate-marquee">
                 Important Update: Sonic{" "}
                 {getNetworkNamesById(maintenanceNetworks).join(" & ")} will
                 upgrade on {format(new UTCDate(maintenanceStartTime), "PPP")},
@@ -332,17 +339,17 @@ export function Header() {
                 . Some tasks and on-chain interactions will be paused. Thank you
                 for understanding.
               </div> */}
-              {/* maintenance */}
-              <div className="pl-[100%] animate-marquee">
-                Important Update: Sonic{" "}
-                {getNetworkNamesById(maintenanceNetworks).join(" & ")} will
-                undergo maintenance on{" "}
-                {format(new UTCDate(maintenanceStartTime), "PPP")}, at{" "}
-                {format(new UTCDate(maintenanceStartTime), "h a")} UTC. Some
-                tasks and on-chain interactions will be paused. Thank you for
-                understanding.
-              </div>
-              {/* <div className="pl-[100%] animate-marquee">
+                {/* maintenance */}
+                <div className="pl-[100%] animate-marquee">
+                  Important Update: Sonic{" "}
+                  {getNetworkNamesById(maintenanceNetworks).join(" & ")} will
+                  undergo maintenance on{" "}
+                  {format(new UTCDate(maintenanceStartTime), "PPP")}, at{" "}
+                  {format(new UTCDate(maintenanceStartTime), "h a")} UTC. Some
+                  tasks and on-chain interactions will be paused. Thank you for
+                  understanding.
+                </div>
+                {/* <div className="pl-[100%] animate-marquee">
                 Sonic Devnet upgrade in progress due to upcoming{" "}
                 <a href="https://www.nodpad.ai/" className="underline mx-1">
                   Node Sale
@@ -350,40 +357,40 @@ export function Header() {
                 traffic. Expected to be completed by the end of the raffle sale.
                 Some tasks will be temporarily unavailable.
               </div> */}
-            </span>
-          </div>
-        </NotificationBar>
-      )}
+              </span>
+            </div>
+          </NotificationBar>
+        )}
 
-      {/* notification bar */}
-      {!isInMaintenance && isOpenLotteryBar && (
-        <NotificationBar className="bg-[#00063C] min-h-12 h-auto px-3">
-          <div className="w-full inline-flex flex-row justify-center items-center gap-1 text-center text-base text-[#48BBFF] font-semibold py-3">
-            <Trophy
-              width={24}
-              height={24}
-              color="#48BBFF"
-              className="min-w-6 min-h-6"
-            />
-            <span className="inline-flex w-full whitespace-nowrap overflow-hidden">
-              Congratulations! Address {formatAddress(bannerMessage?.wallet)}{" "}
-              has won an additional {bannerMessage?.amount}{" "}
-              {bannerMessage?.amount === 1 ? "ring" : "rings"} reward in the{" "}
-              <a href="/task/ring-lottery" className="underline mx-1">
-                ring lottery
-              </a>{" "}
-              about{" "}
-              {formatDistance(new Date(bannerMessage.date), new UTCDate(), {
-                addSuffix: true
-              })}
-              !
-            </span>
-          </div>
-        </NotificationBar>
-      )}
+        {/* notification bar */}
+        {!isInMaintenance && isOpenLotteryBar && (
+          <NotificationBar className="bg-[#00063C] min-h-12 h-auto px-3">
+            <div className="w-full inline-flex flex-row justify-center items-center gap-1 text-center text-base text-[#48BBFF] font-semibold py-3">
+              <Trophy
+                width={24}
+                height={24}
+                color="#48BBFF"
+                className="min-w-6 min-h-6"
+              />
+              <span className="inline-flex w-full whitespace-nowrap overflow-hidden">
+                Congratulations! Address {formatAddress(bannerMessage?.wallet)}{" "}
+                has won an additional {bannerMessage?.amount}{" "}
+                {bannerMessage?.amount === 1 ? "ring" : "rings"} reward in the{" "}
+                <a href="/task/ring-lottery" className="underline mx-1">
+                  ring lottery
+                </a>{" "}
+                about{" "}
+                {formatDistance(new Date(bannerMessage.date), new UTCDate(), {
+                  addSuffix: true
+                })}
+                !
+              </span>
+            </div>
+          </NotificationBar>
+        )}
 
-      {/* news notification bar */}
-      {/* {isInWalletCampaignTime(networkId) && (
+        {/* news notification bar */}
+        {/* {isInWalletCampaignTime(networkId) && (
         <NotificationBar className="bg-[#00063C] min-h-12 h-auto px-3">
           <div className="w-full inline-flex justify-center gap-2 text-base text-[#48BBFF] font-semibold py-3">
             <Speaker
@@ -408,6 +415,7 @@ export function Header() {
           </div>
         </NotificationBar>
       )} */}
-    </nav>
+      </nav>
+    </div>
   );
 }
--- app/task/components/Banner.tsx
+++ app/task/components/Banner.tsx
@@ -1,6 +1,15 @@
 import { MODAL_HASH_MAP, openModalDirectly } from "@/app/hooks/useModalHash";
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPagination
+} from "@/components/ui/carousel";
 import { trackClick } from "@/lib/track";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function Banner() {
   const handleOpenHowToPlayDialog = () => {
@@ -8,8 +17,9 @@ export default function Banner() {
     trackClick({ text: "How to play?" });
   };
   return (
-    <div className="bg-black text-primary">
-      <div className="max-w-view w-full mx-auto px-4 h-[300px] flex items-center">
+    <div className="bg-black text-primary relative">
+      <div className="max-w-view w-full mx-auto px-4 h-[300px] flex items-center relative z-10">
+        {/* left part */}
         <div className="flex flex-col gap-4 max-w-[486px]">
           <div className="font-orbitron text-headline3">
             Odyssey Task Center
@@ -27,15 +37,80 @@ export default function Banner() {
           </p>
           <Button
             onClick={() => handleOpenHowToPlayDialog()}
-            className="text-title3 font-orbitron mt-4 px-6 w-fit"
+            className="text-title3 font-orbitron mt-4 px-6 w-fit hidden md:block"
             variant={"outline"}
           >
             How to Play?
           </Button>
+          <div className="fixed bottom-0 left-0 w-full p-4 z-20 bg-black md:hidden">
+            <Button
+              onClick={() => handleOpenHowToPlayDialog()}
+              className="text-title3 font-orbitron px-6 w-full"
+              variant={"outline"}
+            >
+              How to Play?
+            </Button>
+          </div>
         </div>
-
-        <div></div>
+        <div className="h-full grow flex justify-end items-center">
+          <Carousel
+            className="flex justify-end w-[558px] h-[172px]"
+            plugins={[Autoplay({ delay: 3000 })]}
+          >
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="flex items-center basis-full cursor-pointer group/banner"
+                >
+                  <div className="size-full relative">
+                    <img
+                      className="size-full"
+                      src="/images/banner/banner-1.png"
+                      alt=""
+                    />
+                    <Link
+                      href={"/"}
+                      className={cn(
+                        "flex-center absolute inset-0 rounded border border-link z-10 bg-black/70",
+                        "text-headline4 font-orbitron gap-1 group-hover/banner:opacity-100 opacity-0 transition-opacity duration-300"
+                      )}
+                    >
+                      <PlayLogo />
+                      Start Now
+                    </Link>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPagination
+              className="absolute -bottom-6 left-1/2 -translate-x-1/2 flex gap-2"
+              activeDotClassName="bg-primary"
+            />
+          </Carousel>
+        </div>
+      </div>
+      {/* right part */}
+      <div className="h-full absolute top-0 right-0 w-[912px] h-[300px] z-0">
+        <img className="size-full" src="/images/banner/banner-bg.png" alt="" />
       </div>
     </div>
+  );
+}
+
+function PlayLogo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="39"
+      height="40"
+      viewBox="0 0 39 40"
+      fill="none"
+    >
+      <path
+        d="M13.0996 31.2008V8.80078L30.6996 20.0008L13.0996 31.2008ZM16.2996 25.3608L24.6996 20.0008L16.2996 14.6408V25.3608Z"
+        fill="white"
+      />
+    </svg>
   );
 }
