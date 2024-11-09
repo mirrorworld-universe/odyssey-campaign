ff3c26cb223dc98ca4768558cc2f151259c8426f
jack pan <jackpanyj@gmail.com>
2024-11-09 23:42:21 +0800

    chore: optimize img and cache img

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,14 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/images/(.*)",
+        headers: [{ key: "cache-control", value: "max-age=7200" }]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
