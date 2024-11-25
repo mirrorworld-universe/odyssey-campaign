8ff5502e5c8d5082b6d13e872454cd91a0d303c6
jack pan <jackpanyj@gmail.com>
2024-11-25 11:45:57 +0800

    feat: save

--- app/reward/components/H5Data.tsx
+++ app/reward/components/H5Data.tsx
@@ -22,31 +22,33 @@ export default function H5Data() {
             Season 3 <Tag text="Current" />
           </div>
         </AccordionTrigger>
-        <AccordionContent className={cn("sonic-body3 text-tertary p-0")}>
-          <div className="space-y-2 [&>*]:flex [&>*]:justify-between [&>*]:items-center">
-            <div>
-              <p>Network</p>
-              <p className="text-primary">Frontier V1</p>
-            </div>
-            <div>
-              <p>Start Date (UTC)</p>
-              <p className="text-primary">2024-09-01</p>
-            </div>
-            <div>
-              <p>End Date (UTC)</p>
-              <p className="text-primary">2024-09-30</p>
-            </div>
-            <div>
-              <p>Snapshot Date (UTC)</p>
-              <p className="text-primary">2024-09-15</p>
-            </div>
-            <div>
-              <p>Reward Rings</p>
-              <p className="flex items-center gap-1 text-primary">
-                <Ring width={16} height={16} color="#FBB042" />
-                100
-              </p>
-            </div>
+        <AccordionContent
+          className={cn(
+            "sonic-body3 text-tertary p-0 space-y-2 [&>*]:flex [&>*]:justify-between [&>*]:items-center"
+          )}
+        >
+          <div>
+            <p>Network</p>
+            <p className="text-primary">Frontier V1</p>
+          </div>
+          <div>
+            <p>Start Date (UTC)</p>
+            <p className="text-primary">2024-09-01</p>
+          </div>
+          <div>
+            <p>End Date (UTC)</p>
+            <p className="text-primary">2024-09-30</p>
+          </div>
+          <div>
+            <p>Snapshot Date (UTC)</p>
+            <p className="text-primary">2024-09-15</p>
+          </div>
+          <div>
+            <p>Reward Rings</p>
+            <p className="flex items-center gap-1 text-primary">
+              <Ring width={16} height={16} color="#FBB042" />
+              100
+            </p>
           </div>
         </AccordionContent>
       </AccordionItem>
